eacf01ce3fccef6cf716a6730aef7a56a1763535
r.aramide <r.aramide@lagetronix.com>
2023-11-21 17:07:09 +0100

    done slide 6, remaining 7 and 8

--- app.js
+++ app.js
@@ -2,3 +2,14 @@ function toggleDropdown() {
   const dropdownContainer = document.querySelector(".setup-guide");
   dropdownContainer.classList.toggle("active");
 }
+
+function toggleAccordion(index) {
+  const accordionBoxes = document.querySelectorAll(".accordion-box");
+  accordionBoxes.forEach((item, i) => {
+    if (i === index - 1) {
+      item.classList.toggle("close");
+    } else {
+      item.classList.add("close");
+    }
+  });
+}
